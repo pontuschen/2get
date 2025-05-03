8a22d6a771402d253a31f1c11b7f99f012aa11f1
pontuschen <ponpon0704@gmail.com>
2025-05-03 09:49:20 +0800

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDqMfXPUaks_xU-X7an8KOn47B79sw9W8I",
-  authDomain: "the2get.firebaseapp.com",
-  projectId: "the2get",
-  storageBucket: "the2get.appspot.com",
-  messagingSenderId: "619520361854",
-  appId: "1:619520361854:web:cd94b73559cebe9cdc9f2f",
-  measurementId: "G-TYEB6QFR0W"
-};
